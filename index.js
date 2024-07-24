e2f173347a69ba2b5beaabe8f89b34c9717b1807
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-24 13:18:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config()
+
+const Port = 8000;
+const DatabaseAPI = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017'
+mongoose.set('strictQuery', false)
+
+async function Main(){
+    await mongoose.connect(DatabaseAPI)
+    console.log('connected to database')
+    app.listen(Port , ()=> console.log('Connected to port'))
+}
+
+Main();
